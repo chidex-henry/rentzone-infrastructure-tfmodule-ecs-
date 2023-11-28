24a8a024e5cdba22a5579b35d17416203744cb08
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-11-27 21:51:01 -0600

    file added

--- main.tf
+++ main.tf
@@ -56,3 +56,10 @@ module "rds" {
   multi_az_deployment          = var.multi_az_deployment
   database_security_group_id   = module.security_group.database_security_group_id
 }
+
+# request ssl certificate 
+# module "ssl_certificate" {
+#   source            = "git@github.com:chidex-henry/Terraform-modules.git//acm"
+#   domain_name       = var.domain_name
+#   alternative_names = var.alternative_names
+# }
