5e6091640642ea554a5758dc2adeec581dda448a
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-11-27 21:15:00 -0600

    file added

--- variables.tf
+++ variables.tf
@@ -3,7 +3,6 @@ variable "region" {}
 variable "project_name" {}
 variable "environment" {}
 
-
 #vpc variables 
 variable "vpc_cidr" {}
 variable "public_subnet_az1_cidr" {}
@@ -12,3 +11,9 @@ variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
 variable "private_data_subnet_az2_cidr" {}
+
+#rds variables 
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
